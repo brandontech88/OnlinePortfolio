86f232971c3b9ebce24037929b702e40dfac63fa
Scott <scott@Scotts-MacBook-Pro.local>
2023-01-12 22:41:08 -0500

    first commit

---
+++ pages/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className='py-5 flex bg-gradient-to-r from-cyan-500 to-teal-500 dark:'>
+            <h1 className="text-md text-white font-burtons mx-auto">&copy; Brandon Scott LLC 2023</h1>
+        </div>
+    )
+}
+
+export default Footer;
